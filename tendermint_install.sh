9c177e2d991da563a6aab0c46a3bca5c8bffb06f
zack <zack.bitcoin@gmail.com>
2015-03-20 00:18:50 -0700

    fixed stuff

--- tendermint_install.sh
+++ tendermint_install.sh
@@ -6,12 +6,12 @@ apt-get upgrade -y
 apt-get install -y make screen gcc git mercurial libc6-dev pkg-config libgmp-dev
 mkdir /home/zack
 cd /home/zack
+mkdir gocode
 wget https://storage.googleapis.com/golang/go1.4.2.src.tar.gz
 tar -xzvf go*.tar.gz
 cd go/src
 ./make.bash
 useradd zack -d /home/zack
-#su zack
 cd /home/zack
 cp /etc/skel/.bashrc .
 mkdir -p /home/zack/go/src
@@ -29,9 +29,5 @@ cd $GOPATH/src/github.com/tendermint/tendermint
 git checkout new_evm
 make get_deps
 make
-chown zack /home/zack
-chown zack /home/zack/*
-chown zack /home/zack/*/*
-chown zack /home/zack/*/*/*
-chown zack /home/zack/*/*/*/*
-chown zack /home/zack/*/*/*/*/*
+#chown zack /home/zack
+#chown zack /home/zack/*
